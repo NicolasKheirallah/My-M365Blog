e44cd916912be1b32baac17147b5e47c054daac6
Amazinjoey <Nicolas.Kheirallah@gmail.com>
2020-12-28 23:33:58 +0100

    Some more darkmode fixes

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -17,7 +17,7 @@ const Card = ({
                 <h2 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 dark:text-gray-200 mt-4 mb-4">{title}</h2>
                 <p class="leading-relaxed mb-8 dark:text-gray-200">{description}.</p>
                 <div class="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
-                  <a class="text-gray-500  dark:text-gray-200 inline-flex items-center">Read
+                  <a class="text-gray-500  dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-600 inline-flex items-center">Read
                     <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                       <path d="M5 12h14"></path>
                       <path d="M12 5l7 7-7 7"></path>
--- src/components/navigation/navigationItems.js
+++ src/components/navigation/navigationItems.js
@@ -4,7 +4,7 @@ const NavigationItems = (props) => {
 
     return (
         <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-        <a class="mr-5 hover:text-gray-900" href={props.url}>{props.title}</a>
+        <a class="mr-5 hover:text-gray-900 dark:hover:text-gray-600" href={props.url}>{props.title}</a>
       </nav>
 
     )
