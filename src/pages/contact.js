95f266b5a88f6a8b29569ea8c0575efecaef3a69
Amazinjoey <Nicolas.Kheirallah@gmail.com>
2020-12-28 20:43:40 +0100

    small fix

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,24 +1,24 @@
 import React from "react"
-import { graphql } from "gatsby"
+import {graphql} from "gatsby"
 
 import Layout from "../components/layout"
-import Contact from "../components/Contact/Contact"
+import Contact from "../components/Contact/contact"
 import SEO from "../components/seo"
 
-const ContactMe = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
+const ContactMe = ({data, location}) => {
+    const siteTitle = data.site.siteMetadata.title
 
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="Contact Me" />
-      <Contact/>
-    </Layout>
-  )
+    return (
+        <Layout location={location} title={siteTitle}>
+            <SEO title="Contact Me"/>
+            <Contact/>
+        </Layout>
+    )
 }
 
 export default ContactMe
 
-export const pageQuery = graphql`
+export const pageQuery = graphql `
   query {
     site {
       siteMetadata {
