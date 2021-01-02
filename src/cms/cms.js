00f0d86ffe8059ee878c366437c55a9d64004ac1
Amazinjoey <Nicolas.Kheirallah@gmail.com>
2021-01-02 20:26:30 +0100

    some fixes for netlifycms

---
+++ src/cms/cms.js
@@ -0,0 +1,4 @@
+import CMS from 'netlify-cms-app'
+import BlogPostTemplate from '../templates/blog-post'
+
+CMS.registerPreviewTemplate('blog', BlogPostTemplate)
--- src/components/navigation/navigationSearchBar.js
+++ src/components/navigation/navigationSearchBar.js
@@ -10,7 +10,9 @@ const NavigationSearchBar = (props) => {
                 name="Search-field"
                 placeholder="Search"
                 class="w-full dark:bg-gray-700 bg-opacity-40 rounded border border-gray-700 focus:ring-2 focus:ring-gray-900 focus:bg-transparent focus:border-gray-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      
         </div>
+        
     )
 }
 export default NavigationSearchBar
