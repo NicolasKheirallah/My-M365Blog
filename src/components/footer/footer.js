2d8b7f9ce0f0debe75054c9322115db16345daa8
Amazinjoey <Nicolas.Kheirallah@gmail.com>
2021-01-02 21:25:12 +0100

    Fixed wrong file extension for netlifycms

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,15 +5,15 @@ const Footer = () => {
     return (
         <footer class="text-gray-600 body-font">
             <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-                <a
+                <div
                     class="flex title-font font-medium items-center md:justify-start justify-center text-gray-600 dark:text-gray-200">
                     <span class="ml-3 text-xl">Your Modern Workplace</span>
-                </a>
+                </div>
                 <p
                     class="text-sm text-gray-500 dark:text-gray-200 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 Nicolas Kheirallah</p>
                 <span
                     class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                    <a class="text-gray-500 dark:text-gray-200" href="">
+                    <a class="text-gray-500 dark:text-gray-200" href="https://www.facebook.com/nicholas.kheirallah">
                         <svg fill="currentColor" class="w-5 h-5" viewBox="0 0 24 24">
                             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
