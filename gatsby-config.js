6e177346258b2f3e7f8e5d04abb0de8931a2b542
Amazinjoey <Nicolas.Kheirallah@gmail.com>
2021-01-07 16:37:21 +0100

    added google analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,73 +1,69 @@
 module.exports = {
-  siteMetadata: require("./sitemetadata.json"),
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.mdx`, `.md`],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    siteMetadata: require("./sitemetadata.json"),
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 630,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
+                path: `${__dirname}/content/blog`,
+                name: `blog`
+            }
+        }, {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed-mdx`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `My M365 Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    {
-    resolve: 'gatsby-plugin-netlify-cms',
-    options: {
-      modulePath: `${__dirname}/src/cms/cms.js`,
-    },
-  },
-    `gatsby-plugin-react-helmet`,
-    'gatsby-plugin-postcss',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+                path: `${__dirname}/content/assets`,
+                name: `assets`
+            }
+        }, {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                extensions: [
+                    `.mdx`, `.md`
+                ],
+                gatsbyRemarkPlugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 630
+                        }
+                    }, {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`
+                        }
+                    },
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-smartypants`
+                ]
+            }
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`, {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "G-HBYQV1CWTB",
+                head: true
+            }
+        },
+        `gatsby-plugin-feed-mdx`, {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `My M365 Blog`,
+                short_name: `GatsbyJS`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `content/assets/gatsby-icon.png`
+            }
+        }, {
+            resolve: 'gatsby-plugin-netlify-cms',
+            options: {
+                modulePath: `${__dirname}/src/cms/cms.js`
+            }
+        },
+        `gatsby-plugin-react-helmet`,
+        'gatsby-plugin-postcss',
+        // this (optional) plugin enables Progressive Web App + Offline functionality To
+        // learn more, visit: https://gatsby.dev/offline `gatsby-plugin-offline`,
+    ]
 }
