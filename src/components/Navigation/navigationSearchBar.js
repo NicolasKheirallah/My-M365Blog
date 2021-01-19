dd0b16519b088a663e30c05cb96ad4f708632914
Amazinjoey <Nicolas.Kheirallah@gmail.com>
2021-01-20 00:38:38 +0100

    Fixed styling

--- src/components/Navigation/navigationSearchBar.js
+++ src/components/Navigation/navigationSearchBar.js
@@ -12,7 +12,7 @@ const NavigationSearchBar = () => {
           frontmatter {
             title
             description
-            date(formatString: "DD,MMMM, YYYY")
+            date(formatString: "DD MMMM YYYY")
             category
           }
           fields {
@@ -42,12 +42,6 @@ const NavigationSearchBar = () => {
     const posts = hasSearchResults
         ? filteredData
         : allPosts
-
-        console.log(posts);
-
-        {posts.map(({node}) => {
-          console.log(node)
-          })}
     return (
         <div class="navSearch">
             <input
@@ -56,17 +50,21 @@ const NavigationSearchBar = () => {
                 name="Search-field"
                 placeholder="Search"
                 onChange={handleInputChange}
-                class="dark:bg-gray-700 bg-opacity-40 rounded border border-gray-700 focus:ring-2 focus:shadow-lg text-base outline-none text-gray-100 py-1 px-3 transition-colors duration-200 ease-in-out"/>
-            <div class="navSearchResults container mx-auto w-full justify-between m-2 top-10 absolute h-auto hidden">
-                  <ui>
+                class="dark:bg-gray-700 bg-opacity-40 rounded-lg border border-gray-700 focus:ring-2 focus:shadow-lg text-base outline-none text-gray-100 py-1 px-3 transition-colors duration-200 ease-in-out"/>
+            <div class="navSearchResults flex py-5 justify-between absolute z-40 pr-5">
+                  <div class="overflow-hidden bg-white rounded-lg shadow-lg">
       {posts.map(({node}) => {
                             const {title, date, description, category} = node.frontmatter
                             const {slug} = node.fields
                             return (
-                              <li class="py-1"><a class="block font-bold text-base uppercase cursor-pointer">{title}</a></li>
+                              <a class="block group hover:bg-blue p-4 border-b hover:bg-gray-400" href={slug}>
+                                <p class="font-bold text-md text-black ">{title}</p>
+                                <p class="text-sm text-black">{description}</p>
+                                <p class="text-xs right-0 text-black">{date}</p>
+                                </a>
                             )
                         })}
-                </ui>
+                </div>
       </div>
         </div>
     )
